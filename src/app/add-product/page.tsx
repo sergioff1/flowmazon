9b0817b94b1ea4a37bfecb851b8b2d144414d109
Sergio Ferreira <serfern@msn.com>
2023-11-23 12:05:29 -0500

    fix prisma import in add-product

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,5 +1,5 @@
 import FormSubmitBtn from '@/components/FormSubmitBtn'
-import prisma from '@/lib/db/prisma'
+import {prisma} from '@/lib/db/prisma'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from '../api/auth/[...nextauth]/route'
